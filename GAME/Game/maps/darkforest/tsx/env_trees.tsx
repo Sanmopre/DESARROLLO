4d7c184f4a021bf07c85f6eea606f5294c541561
Dani <45202338+xsiro@users.noreply.github.com>
2019-10-12 15:38:30 +0200

    arreglado mapa en tiled

---
+++ GAME/Game/maps/darkforest/tsx/env_trees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="env_trees" tilewidth="16" tileheight="16" tilecount="1156" columns="34">
+ <image source="darkforest/env_trees.png" width="544" height="544"/>
+</tileset>
