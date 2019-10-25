96eb2fb343b3f6506ed15e4b345fdbcc71a3d75a
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2019-10-25 11:15:34 +0200

    collisions check

--- Desarrollo(VS2017)/Motor2D/j1Collision.cpp
+++ Desarrollo(VS2017)/Motor2D/j1Collision.cpp
@@ -35,50 +35,56 @@ j1Collision::~j1Collision()
 
 bool j1Collision::PreUpdate()
 {
-	bool ret = true;
 	// Remove all colliders scheduled for deletion
-	for(uint i = 0; i < MAX_COLLIDERS; ++i)
+	for (uint i = 0; i < MAX_COLLIDERS; ++i)
 	{
-		if(colliders[i] != nullptr && colliders[i]->to_delete == true)
+		if (colliders[i] != nullptr && colliders[i]->to_delete == true)
 		{
 			delete colliders[i];
 			colliders[i] = nullptr;
 		}
 	}
 
-	// Calculate collisions
 	Collider* c1;
 	Collider* c2;
 
-	for(uint i = 0; i < MAX_COLLIDERS; ++i)
+	for (uint i = 0; i < MAX_COLLIDERS; ++i)
 	{
 		// skip empty colliders
-		if(colliders[i] == nullptr)
+		if (colliders[i] == nullptr)
 			continue;
 
 		c1 = colliders[i];
+		if (c1->Enabled == false)
+		{
+			continue;
+		}
 
 		// avoid checking collisions already checked
-		for(uint k = i+1; k < MAX_COLLIDERS; ++k)
+		for (uint k = i + 1; k < MAX_COLLIDERS; ++k)
 		{
 			// skip empty colliders
-			if(colliders[k] == nullptr)
+			if (colliders[k] == nullptr)
 				continue;
 
 			c2 = colliders[k];
-
-			if(c1->CheckCollision(c2->rect) == true)
+			if (c2->Enabled == false)
 			{
-				if(matrix[c1->type][c2->type] && c1->callback) 
+				continue;
+			}
+
+			if (c1->CheckCollision(c2->rect) == true)
+			{
+				if (matrix[c1->type][c2->type] && c1->callback)
 					c1->callback->OnCollision(c1, c2);
-				
-				if(matrix[c2->type][c1->type] && c2->callback) 
+
+				if (matrix[c2->type][c1->type] && c2->callback)
 					c2->callback->OnCollision(c2, c1);
 			}
 		}
 	}
 
-	return ret;
+	return true;
 }
 
 // Called before render is available
@@ -93,7 +99,7 @@ bool j1Collision::Update()
 
 void j1Collision::DebugDraw()
 {
-	if(App->input->GetKey(SDL_SCANCODE_F1) == KEY_DOWN)
+	if(App->input->GetKey(SDL_SCANCODE_F9) == KEY_DOWN)
 		debug = !debug;
 
 	if(debug == false)
