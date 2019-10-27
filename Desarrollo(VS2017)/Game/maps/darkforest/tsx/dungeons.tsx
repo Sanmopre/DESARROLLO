341c613595a74b990f00c949aebffa91813252b4
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2019-10-27 21:47:20 +0100

    shen

--- Desarrollo(VS2017)/Game/maps/darkforest/tsx/dungeons.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.5" name="dungeons" tilewidth="16" tileheight="16" tilecount="216" columns="24">
- <image source="tileset.png" width="384" height="144"/>
-</tileset>
--- Desarrollo(VS2017)/Game/maps/tsx/env_mon.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.5" name="env_mon" tilewidth="16" tileheight="16" tilecount="900" columns="30">
- <image source="darkforest/env_mon.png" width="480" height="480"/>
-</tileset>
