5abda5e039e069e0fd0cb762f9ef8f8281869a10
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2019-12-28 11:39:58 +0100

    added debug UI Quads

--- Desarrollo(VS2017)/Motor2D/j1GUIelement.cpp
+++ Desarrollo(VS2017)/Motor2D/j1GUIelement.cpp
@@ -49,7 +49,7 @@ void j1GUIelement::Draw()
 	}
 
 	App->render->Blit_UI(texture, globalPosition.x, globalPosition.y, &rect, 0.0f, 0.0f);
-
+	App->render->DrawQuad({ globalPosition.x, globalPosition.y, rect.w, rect.h }, 0, 255, 255, 255, false, false, true);
 	
 }
 
